6cfa10f2772878165d3d0d3f861432723888d90b
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-09-27 21:47:21 +0200

    added assertations

--- tests/registerAndLogin.spec.js
+++ tests/registerAndLogin.spec.js
@@ -4,9 +4,8 @@ const { RegisterPage } = require('../POM/ui/registerPage');
 const { LoginPage } = require('../POM/ui/loginPage');
 
 test.describe('Register and Login successfully', () => {
-  //Get valid user data
+  //Get random valid user data
   const { username, email, password } = userData.validUser();
-
   test.beforeEach('Visit Home Page', async ({ page }) => {
     await page.goto('https://automaticityacademy.ngrok.app/');
   });
@@ -14,11 +13,23 @@ test.describe('Register and Login successfully', () => {
   test('Register Successfully', async ({ page }) => {
     const onRegisterPage = new RegisterPage(page);
     await onRegisterPage.registerValidUser(username, email, password);
+    await expect(page.locator('label', { hasText: 'Username' })).toBeVisible();
+    await expect(
+      page.locator('span', { hasText: 'Already have an account?' })
+    ).toBeVisible();
+    await expect(page.getByText('Successfully registered!')).toBeVisible();
+    await expect(
+      page.locator('span', { hasText: 'Buy some stuff bruh' })
+    ).toBeVisible();
   });
 
   test('Login Successfull', async ({ page }) => {
     const onLoginPage = new LoginPage(page);
     await onLoginPage.loginValidUser(email, password);
+    await expect(
+      page.locator('span', { hasText: 'Buy some stuff bruh' })
+    ).toBeVisible();
+    await expect(page.getByText('Today deals:')).toBeVisible();
   });
 
   test.afterEach('Logout', async ({ page }) => {
