dcffa6f2faf0c2570f6fd3d6b073c2afb21f6239
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-09-27 23:44:36 +0200

    fixed assertions to capture frame

--- tests/registerAndLogin.spec.js
+++ tests/registerAndLogin.spec.js
@@ -4,15 +4,23 @@ const { RegisterPage } = require('../POM/ui/registerPage');
 const { LoginPage } = require('../POM/ui/loginPage');
 
 test.describe('Register and Login successfully', () => {
-  //Get random valid user data
-  const { username, email, password } = userData.validUser();
+  const validUsername = `aleksa${Math.floor(Math.random() * 100000) + 1}`;
+  const validEmail = `aleksa+${
+    Math.floor(Math.random() * 100000) + 1
+  }@gmail.com`;
+  const validPassword = 'test123';
+
   test.beforeEach('Visit Home Page', async ({ page }) => {
     await page.goto('https://automaticityacademy.ngrok.app/');
   });
 
   test('Register Successfully', async ({ page }) => {
     const onRegisterPage = new RegisterPage(page);
-    await onRegisterPage.registerValidUser(username, email, password);
+    await onRegisterPage.registerValidUser(
+      validUsername,
+      validEmail,
+      validPassword
+    );
     await expect(page.locator('label', { hasText: 'Username' })).toBeVisible();
     await expect(
       page.locator('span', { hasText: 'Already have an account?' })
@@ -21,15 +29,19 @@ test.describe('Register and Login successfully', () => {
     await expect(
       page.locator('span', { hasText: 'Buy some stuff bruh' })
     ).toBeVisible();
+    const frame = page.frameLocator('iframe');
+    expect(frame.locator('h4', { hasText: 'Today deals:' })).toBeVisible();
   });
 
   test('Login Successfull', async ({ page }) => {
     const onLoginPage = new LoginPage(page);
-    await onLoginPage.loginValidUser(email, password);
+    await onLoginPage.loginValidUser(validEmail, validPassword);
+
     await expect(
       page.locator('span', { hasText: 'Buy some stuff bruh' })
     ).toBeVisible();
-    await expect(page.getByText('Today deals:')).toBeVisible();
+    const frame = page.frameLocator('iframe');
+    expect(frame.locator('h4', { hasText: 'Today deals:' })).toBeVisible();
   });
 
   test.afterEach('Logout', async ({ page }) => {
