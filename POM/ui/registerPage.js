ed49cc10291f69538fdf9c62e660f472fac13a0c
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-09-27 20:52:39 +0200

    added registerValidUser method

--- POM/ui/registerPage.js
+++ POM/ui/registerPage.js
@@ -1,4 +1,16 @@
 const { test } = require('@playwright/test');
 const { BaseHomePage } = require('./baseHomePage');
 
-exports.RegisterPage = class RegisterPage extends BaseHomePage {};
+exports.RegisterPage = class RegisterPage extends BaseHomePage {
+  constructor(page) {
+    super(page);
+  }
+
+  async registerValidUser(username, email, password) {
+    await this.signUpButton.click();
+    await this.page.locator('#username').fill(username);
+    await this.page.locator('#email').fill(email);
+    await this.page.locator('#password').fill(password);
+    await this.page.getByRole('button', { name: 'Register' }).click();
+  }
+};
