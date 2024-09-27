18ffdf4990d9521ea5523ef0625bb48789ee7813
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-09-27 17:39:39 +0200

    added after each for logout functionality

--- tests/registerAndLogin.spec.js
+++ tests/registerAndLogin.spec.js
@@ -7,9 +7,14 @@ test.describe('Register and Login successfully', () => {
 
   test('Register Successfully', async ({ page }) => {
     await page.getByText('Sign up').click();
-    await page.locator('#username').fill('testusername1');
-    await page.locator('#email').fill('testemail1@test.com');
+    await page.locator('#username').fill('te2s2112t212use21rname1');
+    await page.locator('#email').fill('teste21ma21222i121l1@21test.com');
     await page.locator('#password').fill('testpass');
     await page.locator('button').getByText('Register').click();
+  });
+
+  test.afterEach('Logout', async ({ page }) => {
+    await page.locator('.relative > div > span > .inline-flex').click();
+    await page.getByText('Log Out').click();
   });
 });
