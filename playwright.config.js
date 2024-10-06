4bbef05f4ffbe217e789cb0c26d323ed145cb920
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-10-06 23:42:08 +0200

    returned worker number,added slow tests

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -28,6 +28,7 @@ export default defineConfig({
     baseURL: 'https://automaticityacademy.ngrok.app',
     //Added test ID attribute
     testIdAttribute: 'test-id',
+    /*ti
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
