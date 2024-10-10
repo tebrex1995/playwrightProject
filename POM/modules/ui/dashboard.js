798e1de96770745a8c12373b0c9fb38b5dfc53db
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-10-10 13:07:03 +0200

    changed node version

--- POM/modules/ui/dashboard.js
+++ POM/modules/ui/dashboard.js
@@ -89,7 +89,8 @@ export class Dashboard {
   async navigateToPage(page, productsPage) {
     await page.locator(`button[aria-label="${productsPage}"]`).click();
     //Wait for products to load
-    // await page.waitForTimeout(3000);
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForTimeout(3000);
   }
   //Get number of pages
   async getAllPages(page) {
