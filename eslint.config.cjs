bc816f24f04855f6fcc921e0293fd8ce3b48cae2
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-10-10 13:30:45 +0200

    unistalled babel

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,21 +1,9 @@
 const { configs } = require('eslint-plugin-playwright');
-const babelParser = require('@babel/eslint-parser');
 
 module.exports = [
   {
     ...configs['flat/recommended'],
     files: ['tests/**'],
-    languageOptions: {
-      parser: babelParser,
-      parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ['@babel/preset-env'],
-        },
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-    },
   },
   {
     files: ['tests/**'],
